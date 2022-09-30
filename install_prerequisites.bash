31e3889e82ecdfbb9254d56fa8157a3f053b6a4d
Long <lwuai@connect.ust.hk>
2022-09-30 13:05:30 +0800

    remove depebndency

--- install_prerequisites.bash
+++ install_prerequisites.bash
@@ -1,6 +1,7 @@
 # part one
-sudo apt-get install sqlite3 libsqlite3-dev libcgicc-dev liblog4cpp5-dev libboost-all-dev libfcgi-dev spawn-fcgi nginx curl  gnuplot gnuplot-qt  
-
-# part two: install python related packages.
-sudo apt-get install python3.8-dev swig python-numpy python-all-dev python3-all-dev liblapack-dev
+sudo apt install cmake build-essential libfcgi-dev liblapack-dev
+#sudo apt-get install sqlite3 libsqlite3-dev libcgicc-dev liblog4cpp5-dev libboost-all-dev libfcgi-dev spawn-fcgi nginx curl  gnuplot gnuplot-qt
+#
+## part two: install python related packages.
+#sudo apt-get install python3.8-dev swig python-numpy python-all-dev python3-all-dev liblapack-dev
 
