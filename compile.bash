e45b74872555e33acc938a3ae39c8cec6fe4bcef
Long <lwuai@connect.ust.hk>
2023-02-02 11:44:09 +0800

    update

--- compile.bash
+++ compile.bash
@@ -10,18 +10,22 @@ fi
 echo "compile with GPU: ${with_GPU}"
 
 # to continue.
+releasePath=cmake-build-release-cpu
 if [ ${with_GPU} = "TRUE" ]; then
     echo "compile GPU version";
+    releasePath=cmake-build-release-gpu
 elif [ ${with_GPU} = "FALSE" ]; then 
     echo "compile CPU version";
+    releasePath=cmake-build-release-cpu
 else 
     echo "invalid option of WITH_GPU. It must be TRUE or FALSE. "
 fi
 
-releasePath=$(pwd)
-echo $releasePath
-mkdir cmake-build-release
-cd ${releasePath}/cmake-build-release/ 
+currentPath=$(pwd)
+echo $currentPath
+
+mkdir ${releasePath}
+cd ${currentPath}/${releasePath}/ 
  rm CMakeCache.txt
 
 #source ~/anaconda3/etc/profile.d/conda.sh
@@ -32,14 +36,14 @@ cd ${releasePath}/cmake-build-release/
 # THANKS: https://stackoverflow.com/questions/26491948/how-to-use-gprof-with-cmake
 # -DCMAKE_CXX_FLAGS=-pg -DCMAKE_EXE_LINKER_FLAGS=-pg -DCMAKE_SHARED_LINKER_FLAGS=-pg
 #--debug-output -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON
-cmake -DWITH_GPU=${with_GPU}  -DCMAKE_INSTALL_PREFIX=${releasePath}/build ..
+cmake -DWITH_GPU=${with_GPU}  -DCMAKE_INSTALL_PREFIX=${currentPath}/build ..
 #/usr/local/spectralarchive ..
 # cmake -DCMAKE_INSTALL_PREFIX=/usr/local/spectralarchive --graphviz=foo.dot ..
 # cmake ..
 
 
-cmake  --build ../cmake-build-release   --target  boost  -- -j 30
-cmake  --build ../cmake-build-release  --target spectroscape -- -j 30
+cmake  --build ../${releasePath}   --target  boost  -- -j 30
+cmake  --build ../${releasePath}  --target spectroscape -- -j 30
 
 cmake --install .
 
