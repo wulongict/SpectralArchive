e9f6a1528094b0ac78b9d087bade77f3a2ab9a62
Long <lwuai@connect.ust.hk>
2023-05-25 20:04:13 +0800

    update docker image version v1.1.3

--- docker/ubuntu2004/create_docker_and_run.bash
+++ docker/ubuntu2004/create_docker_and_run.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # create the docker
-docker build -t spectroscape-v1.1.2 .
+docker build -t spectroscape-v1.1.3 .
 
 # run spectroscape
-docker run -it spectroscape-v1.1.2:latest spectroscape 
+docker run -it spectroscape-v1.1.3:latest spectroscape 
 
 # run the docker with bash
-docker run -it spectroscape-v1.1.2:latest bash
+docker run -it spectroscape-v1.1.3:latest bash
 
