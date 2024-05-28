5e7764bd8c1a58b36f0c18477bc15885dec21d2b
Long <lwuai@connect.ust.hk>
2024-05-28 09:51:53 -0500

    compile script updated

--- compile.bash
+++ compile.bash
@@ -1,41 +1,98 @@
 #!/bin/bash
 set -e
-echo "Usage: "$0 "[with_GPU] [build_type]"
-echo "A script for compile spectroscape with CMake"
-echo ""
-echo "if with_GPU is TRUE, compile GPU version, otherwise, CPU version; Default to CPU."
-echo "if build_type is release, compile release version, otherwise, debug version, Default to release. "
-echo ""
-echo "............................. "
-echo $0 $@
-echo "............................. "
-echo ""
-build_type=release
-if (( $# > 1)); then 
-    build_type=$2
-    if [ ${build_type} = "release" ]; then
-        echo "compile release version";
-    elif [ ${build_type} = "debug" ]; then
-        echo "compile debug version";
-    else 
-        echo "invalid option of build_type. Default to release. "
-    fi
-fi
 
-releasePath=cmake-build-${build_type}-cpu
+show_help() {
+    echo "Usage: $0 [option...] " 
+    echo "A script for compile spectroscape with CMake"
+    echo ""
+    echo "Options:"
+    echo "  -g, --gpu           compile GPU version, otherwise, CPU version; Default to CPU."
+    echo "  -b, --build-type    compile release version, otherwise, debug version, Default to release. "
+    echo "  -c, --cores         number of cores to use for compilation. Default to nproc / 16 + 1"
+    echo "  -h, --help          display this help and exit"
+    echo ""
+    echo "Examples:"
+    echo "  $0 --gpu TRUE --build-type release"
+    echo "  $0 --gpu FALSE --build-type debug"
+    echo "  $0 --gpu TRUE --build-type release --cores 8"
+    exit 1
+}
+
+POSITIONAL_ARGS=()
 with_GPU=FALSE
-if (( $# > 0 )); then
-    with_GPU=$1
-    if [ ${with_GPU} = "TRUE" ]; then
-        echo "compile GPU version";
-        releasePath=cmake-build-${build_type}-gpu
-    elif [ ${with_GPU} = "FALSE" ]; then 
-        echo "compile CPU version";
-        releasePath=cmake-build-${build_type}-cpu
-    else 
-        echo "invalid option of WITH_GPU. Default to CPU. "
-    fi
-fi
+build_type=release
+cores=`nproc`
+path_suffix="cpu"
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -g|--gpu)
+      with_GPU="$2"
+      if [ ${with_GPU} = "TRUE" ]; then
+          echo "compile GPU version";
+          path_suffix="gpu"
+      elif [ ${with_GPU} = "FALSE" ]; then 
+          echo "compile CPU version";
+          path_suffix="cpu"
+      else 
+          echo "invalid option of with_GPU. Default to CPU. "
+          with_GPU=FALSE
+          path_suffix="cpu"
+      fi
+      shift # past argument
+      shift # past value
+      ;;
+    -b|--build-type)
+      build_type="$2"
+        if [ ${build_type} = "release" ]; then
+            echo "compile release version";
+        elif [ ${build_type} = "debug" ]; then
+            echo "compile debug version";
+        else 
+            echo "invalid option of build_type. Default to release. "
+            build_type=release
+        fi
+      shift # past argument
+      shift # past value
+      ;;
+    -c|--cores)
+      cores="$2"
+      # limit core number to 1 to 128
+        if ((cores < 1)); then
+            cores=1
+        elif ((cores > 128)); then
+            cores=128
+        fi
+      shift # past argument
+      shift # past value
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      # show help
+      show_help
+      exit 1
+      ;;
+    *)
+      POSITIONAL_ARGS+=("$1") # save positional arg
+      shift # past argument
+      ;;
+  esac
+done
+
+set -- "${POSITIONAL_ARGS[@]}" # restore positional parameters
+
+echo "............................. "
+echo "with_GPU  = ${with_GPU}"
+echo "build_type= ${build_type}"
+echo "cores     = ${cores}"
+echo "POSITIONAL_ARGS = ${POSITIONAL_ARGS}"
+echo "............................."
+
+
+releasePath=cmake-build-${build_type}-${path_suffix}
+echo "releasePath = ${releasePath}"
+
+
+exit 0
 
 currentPath=$(pwd)
 echo $currentPath
@@ -69,9 +126,6 @@ cmake ${cmake_build_options} ..
 # cmake  --graphviz=foo.dot ..
 # cmake ..
 
-# compile libfcgi will multiple cores will crash on macos+docker(ubuntu2204) environment. 
-# it turns out that this is a resource issue on macos.
-cores=$((`nproc` / 16 + 1)) 
 cmake  --build ../${releasePath}  --target spectroscape msmstest using_all_cpu_cores -- -j $cores
 
 # run tests added into CMakeLists.txt file with add_test(Name XXX Command YYY)
