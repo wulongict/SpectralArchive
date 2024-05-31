4d7ed1f4ac1e9bf640c5e7483dbacd45470e85b6
Long <lwuai@connect.ust.hk>
2024-05-31 16:21:24 -0500

    docker file updated.

--- docker/ubuntu2204/create_docker_and_run.bash
+++ docker/ubuntu2204/create_docker_and_run.bash
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # create the docker
-docker build -t spectroscape-v1.1.9 .
+docker build -t spectroscape-v1.2.0 .
 
 # run spectroscape
-docker run -it --rm  spectroscape-v1.1.9:latest spectroscape 
+docker run -it --rm  spectroscape-v1.2.0:latest spectroscape 
 output=$?
 status="compiled successfully;"
 if [ $output -eq 0 ]; then
     echo "spectroscape run successfully"
     # run the docker with bash
-    echo docker run -it --rm spectroscape-v1.1.9:latest bash
+    echo docker run -it --rm spectroscape-v1.2.0:latest bash
 else
     status="compiled failed;"
     echo "spectroscape run failed"
